c9fa0cad81404c27d7439a552fff8ffadc181092
isin <jon.s124524@gmail.com>
2024-01-27 16:49:05 +0200

    feat: create routes initial settings

--- src/pages/PersonalAccount/PersonalAccount.tsx
+++ src/pages/PersonalAccount/PersonalAccount.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
-import './App.scss'
+import '../../App.scss'
 import ProtectedRoute from '../../routes/ProtectedRouteAdmin'
 
 const PersonalAccount: React.FC = () => (

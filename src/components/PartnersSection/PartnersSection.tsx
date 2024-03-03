f707e9a70debf3f91c461d078e354d257117aca5
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2024-03-03 20:55:45 +0200

    feature/create-partners-section-init

---
+++ src/components/PartnersSection/PartnersSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './PartnersSection.module.scss'
+
+const PartnersSection: React.FC = () => {
+  return (
+    <section className={s.partnersSection} id="partnerssection">
+      <div className={s.contentWrapper}></div>
+    </section>
+  )
+}
+
+export default PartnersSection

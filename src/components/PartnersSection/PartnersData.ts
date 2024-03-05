ed8655f5c2740458430c008158650c67b4f1a51c
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2024-03-05 13:41:06 +0200

    feature/create-partners-section-slider-added

---
+++ src/components/PartnersSection/PartnersData.ts
@@ -0,0 +1,62 @@
+export const partnersData = [
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  },
+  {
+    image: 'logo-1.svg',
+    title: 'COMPANY'
+  }
+]
